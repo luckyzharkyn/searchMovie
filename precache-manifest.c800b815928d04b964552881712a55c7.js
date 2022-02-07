28dbea41cd85cb4bc88a70ce3f0f51ef9d9adb4d
Zharkyn Markabaev <Lucky_zharkyn@mail.ru>
2022-02-07 21:58:36 +0600

    Updates

---
+++ precache-manifest.c800b815928d04b964552881712a55c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0e0a99bcfb871cca9895b4c36faba0",
+    "url": "/searchMovie/index.html"
+  },
+  {
+    "revision": "30dd5a09f95d0bdf9447",
+    "url": "/searchMovie/static/css/main.27be6563.chunk.css"
+  },
+  {
+    "revision": "fe4dec88b4311d4cc60d",
+    "url": "/searchMovie/static/js/2.fd08b29f.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/searchMovie/static/js/2.fd08b29f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30dd5a09f95d0bdf9447",
+    "url": "/searchMovie/static/js/main.7459189b.chunk.js"
+  },
+  {
+    "revision": "7d11ab000ed060c3d03c",
+    "url": "/searchMovie/static/js/runtime-main.689c6ca2.js"
+  }
+]);
